005d8cd983d63c2e7ab3c9a5e58281ff3add21e7
Yehor0110 <mcshka111@gmail.com>
2016-10-16 04:19:32 +0300

    Update helpers.c

--- helpers.c
+++ helpers.c
@@ -11,21 +11,24 @@
 /**
  * Возвращает True, если значение в массиве из N значений, иначе false.
  */
-bool search(int value, int values[], int n)
+bool search(int value, int values[], int n) 
 {
-    if (n == 0)
-        return false;
-
-    int midl = n / 2;
     
-    if (values[midl] == value)
-        return true;
-    
-    else if (values[midl] < value)
-        return search (value, values + midl + 1, n - midl - 1);
-        
-    else 
-        return search (value, values, midl);
+    int min = 0;
+    int max = n - 1;
+   
+    while (max >= min) {
+        int midl = (max + min) / 2;
+            if (values[midl] == value)
+                return true;
+            if (min == max && values[midl] != value)
+                return false;
+            else if (values[midl] < value)
+                min = midl + 1;
+            else
+                max = midl - 1;
+    }
+    return false;
 }
 
 /**
@@ -36,9 +39,9 @@ void sort(int values[], int n)
 {
     // реализовать O(n^2) алгоритм сортировки
     
-    for (int i = 0; i < n - 1; i++){
+    for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++)
-            if (values[j] > values[j + 1]){
+            if (values[j] > values[j + 1]) {
                 int tmp = values[j];
                 values[j] = values[j + 1];
                 values[j + 1] = tmp;
